2fe3d4b6054eaca3ff4b5d53b25c510c2fbc0182
Priyanshu Agrawal Macbook Pro <priyanshu06102004@gmail.com>
2025-09-11 01:12:29 +0530

    Cleanup for submission 2

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
-#   bash Task0/tokenize_corpus.sh Data/Doc Data/stopwords.txt temp --time 
+# bash tokenize_corpus.sh Data/Doc Data/stopwords.txt temp --time
 
+# bash build_index.sh Data/Doc temp/vocab.txt temp/out_index --time
 
-# bash Task1/build_index.sh Data/Doc temp/vocab.txt temp/out_index --time
+# bash phrase_search.sh temp/out_index Data/CORD19/queries.json temp/output Data/stopwords.txt --time
 
-# bash Task2/phrase_search.sh temp/out_index Data/CORD19/queries.json temp/output Data/stopwords.txt --time
+# bash vsm.sh temp/out_index Data/CORD19/queries.json temp/results Data/stopwords.txt 100 --time
 
-# bash Task3/vsm.sh temp/out_index Data/CORD19/queries.json temp/results Data/stopwords.txt 100 --time
-
-# bash Task4/bm25_retrieval.sh temp/out_index Data/CORD19/queries.json temp/output Data/stopwords.txt 100 --time
+# bash bm25_retrieval.sh temp/out_index Data/CORD19/queries.json temp/output Data/stopwords.txt 100 --time
